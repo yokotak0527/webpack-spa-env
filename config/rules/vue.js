9034648c4af90724523dc492a2bbab112ce6882b
Yokota Kenji <mail@yokotakenji.me>
2017-05-14 23:46:48 +0900

    中断

--- config/rules/vue.js
+++ config/rules/vue.js
@@ -1,14 +1,21 @@
-const path        = require('path');
-let sassRule      = require('./sass.js');
-sassRule.use.unshift({ loader : 'vue-style-loader' });
+const path           = require('path');
+const styleRule      = require('./style');
+
+// styleRule.use.shift();
+// styleRule.use.unshift({ loader : 'vue-style-loader' });
+
+// console.log(styleRule.use);
+// let qs = combine(styleRule.use);
+// let preloaderQs = combine([styleRule.use.pop()]);
+// let loaderQs    = combine(styleRule.use);
 
 module.exports = {
   loader  : 'vue-loader',
   exclude : /(node_modules|bower_components)/,
   test    : /\.vue$/,
-  // options : {
-  //   loaders : {
-  //     sass : sassRule.use
-  //   }
-  // }
+  options : {
+    loaders : {
+      // postcss :
+    }
+  }
 }
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,13 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import style from 'styles/style.sass';
 
-// let comp = Object.create(null);
-// console.log(comp);
-
-import compHeader from 'components/header.vue';
-// console.log(comp.header);
-// console.log(headerComponent);
+import style from 'styles/style.css';
 
 let app = new Vue({
   el   : '#app',
@@ -18,22 +12,3 @@ let app = new Vue({
     test : compHeader
   }
 });
-// var app = new Vue({
-//   el : '#app',
-//   data : {
-//     message : 'Hello Vue!'
-//   }
-// })
-
-// console.log(VueRouter);
-
-// new Vue()
-// import style from 'styles/style.sass';
-// console.log(style);
-// import Vue from 'vue';
-// import App from '../components/App.vue';
-//
-// new Vue({
-//   el: '#app',
-//   render: h => h(App)
-// });
