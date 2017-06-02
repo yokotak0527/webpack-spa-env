33f4654d460a78727d0addba38e5465165649978
Yokota Kenji <mail@yokotakenji.me>
2017-06-02 15:28:04 +0900

    中断

--- rules/vue.js
+++ rules/vue.js
@@ -3,5 +3,10 @@ const path = require('path');
 module.exports = {
   test    : /\.vue$/,
   loader  : 'vue-loader',
-  exclude : /(node_modules|bower_components)/
+  exclude : /(node_modules|bower_components)/,
+  options : {
+    html : {
+      
+    }
+  }
 }
